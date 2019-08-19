444428f97f66089895f2d0e81a48dc5478a92b43
Hayden Hastings <haydenhastings32@gmail.com>
2019-08-19 15:33:21 -0600

    Added 2 View controller files and completed the storyboard.

---
+++ outaTime/outaTime/View Controllers/DatePickerViewController.swift
@@ -0,0 +1,28 @@
+//
+//  DatePickerViewController.swift
+//  outaTime
+//
+//  Created by Hayden Hastings on 8/19/19.
+//  Copyright © 2019 Hayden Hastings. All rights reserved.
+//
+
+import UIKit
+
+class DatePickerViewController: UIViewController {
+
+    @IBOutlet weak var timeDatePicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    // MARK: - IBActions
+    @IBAction func doneButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func cancelButtonTapped(_ sender: Any) {
+    }
+    
+}
